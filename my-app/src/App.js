2e0bb1609d5a63c528c54e0db674f4aa917f0a57
Mikael Andersson <mikael.andersson@volumental.com>
2016-10-25 14:42:41 +0200

    react test bed initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,387 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Mikael',
+  lastName: 'Andersson',
+  avatarUrl: ''
+};
+
+class Author extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Author: {formatName(user)}!</p>
+                <img src={user.avatarUrl} alt="avatar" />
+            </div>
+        );
+    }
+};
+
+class Greeter extends React.Component {
+    componentDidMount() {
+        console.log("Did mount Greeter");
+    }
+
+    componentWillUnmount() {
+        console.log("Did unmount Greeter");
+    }
+
+    render() {
+        return (
+            <h1>Hello, {this.props.name}</h1>
+        );
+    }
+};
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+        {number}
+    </li>
+);
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert("Text field value is: '" + this.state.value + "'");
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text"
+                       placeholder="Hello!"
+                       value={this.state.value}
+                       onChange={this.handleChange} />
+                <button onClick={this.handleSubmit}>Submit</button>
+            </div>
+        );
+    }
+}
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(value, convert) {
+    const input = parseFloat(value);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    } else {
+        return <p>The water would not boil.</p>;
+    }
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onChange(e.target.value);
+    }
+
+    render() {
+        const value = this.props.value;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={value}
+                       onChange={this.handleChange} />
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {value: '', scale: 'c'};
+    }
+
+    handleCelsiusChange(value) {
+        this.setState({scale: 'c', value});
+    }
+
+    handleFahrenheitChange(value) {
+        this.setState({scale: 'f', value});
+    }
+
+    render() {
+        const scale = this.state.scale;
+        const value = this.state.value;
+        const celsius = scale === 'f' ? tryConvert(value, toCelsius) : value;
+        const fahrenheit = scale === 'c' ? tryConvert(value, toFahrenheit) : value;
+
+        return (
+            <div>
+                <TemperatureInput
+                    scale="c"
+                    value={celsius}
+                    onChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="f"
+                    value={fahrenheit}
+                    onChange={this.handleFahrenheitChange} />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+
+class ProductCategoryRow extends React.Component {
+    render() {
+        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+    }
+}
+
+class ProductRow extends React.Component {
+    render() {
+        var name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>;
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+}
+
+class ProductTable extends React.Component {
+    render() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach((product) => {
+            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+                return;
+            }
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.props.onUserInput(
+            this.refs.filterTextInput.value,
+            this.refs.inStockOnlyInput.checked
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    ref="filterTextInput"
+                    onChange={this.handleChange}
+                />
+                <p>
+                    <input
+                        type="checkbox"
+                        checked={this.props.inStockOnly}
+                        ref="inStockOnlyInput"
+                        onChange={this.handleChange}
+                    />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+}
+
+class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+
+    handleUserInput(filterText, inStockOnly) {
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onUserInput={this.handleUserInput}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+            </div>
+        );
+    }
+}
+
+var PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>{listItems}</ul>
+        <Author />
+          <Clock />
+          <Clock />
+          <Clock />
+          <Toggle />
+          <Form />
+          <Calculator />
+          <FilterableProductTable products={PRODUCTS} />
+          <Greeter name="Kalle" />
+      </div>
+    );
+  }
+}
+
+export default App;
